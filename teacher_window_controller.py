db2769035d24e2c5681c976b365f9277c24d13da
Anely <an.dusaeva@gmail.com>
2021-05-11 20:05:54 +0300

    реализиван вывод функций и представлений

--- teacher_window_controller.py
+++ teacher_window_controller.py
@@ -31,7 +31,78 @@ class Teacher_window(QtWidgets.QMainWindow):
         self.ui.teacherNameComboBox.currentIndexChanged.connect(self.studentName_changed)
         self.ui.SubjectComboBox.currentIndexChanged.connect(self.studentName_changed)
         self.ui.addMarkPushButton.clicked.connect(self.addMarkClicked)
+        self.ui.copyGroupButton.clicked.connect(self.copyGroupClicked)
+        self.ui.turnGroupBackButton.clicked.connect(self.turnGroupBackClicked)
+        self.ui.yearComboBox.currentIndexChanged.connect(self.yearChanged)
         self.show()
+        self.ui.avgMarksInIntervalButton.clicked.connect(self.avgMarksInIntervalClicked)
+        self.ui.saveIntervalButton.clicked.connect(self.saveIntervalClicked)
+
+    def avgMarksInIntervalClicked(self):
+        beginDate = self.ui.beginDateEdit.date()
+        endDate = self.ui.endDateEdit.date()
+        beginDate  = beginDate.toString("yyyy-MM-dd")
+        endDate = endDate.toString("yyyy-MM-dd")
+
+        self.db.cursor.execute("select s.name, round(avg_marks, 3) from intervalAvgMarks('{}'::timestamp, '{}'::timestamp)"
+                           "left join subject s on s.id = subject_id".format(beginDate, endDate))
+        filler.fillTable(self.ui.yearSubjectTableWidget, self.db.cursor, 2)
+
+    def saveIntervalClicked(self):
+        beginDate = self.ui.beginDateEdit.date()
+        endDate = self.ui.endDateEdit.date()
+        beginDate  = beginDate.toString("yyyy-MM-dd")
+        endDate = endDate.toString("yyyy-MM-dd")
+        query = "select s.name, round(avg_marks, 3) from intervalAvgMarks('{}'::timestamp, '{}'::timestamp) " \
+                "left join subject s on s.id = subject_id".format(beginDate, endDate)
+        outputquery = "COPY ({}) TO STDOUT WITH CSV HEADER".format(query)
+        with open('resultsfile.csv', 'w') as f:
+            self.db.cursor.copy_expert(outputquery, f)
+
+
+
+    def yearChanged(self):
+        self.ui.yearSubjectTableWidget.clear()
+        year = self.ui.yearComboBox.currentText()
+        self.db.cursor.execute("select s.name, avg_marks from subjectsAndAvgMarkInOneYear('{}')"
+                               "left join subject s on s.id = subject_id".format(year))
+        filler.fillTable(self.ui.yearSubjectTableWidget, self.db.cursor, 2)
+
+
+    def turnGroupBackClicked(self):
+        number = list(self.ui.groupPointerComboBox2.currentText())
+        new_number = number.copy()
+        new_number[0] = str(int(new_number[0]) + 1)
+        str_new_number = ''
+        str_number = ''
+        for i in new_number:
+            str_new_number += i
+
+        for i in number:
+            str_number += i
+        self.db.cursor.execute("Delete from people  WHERE group_id = (SELECT id from groups where name = '{}');"
+                               "Delete from groups where name = '{}';".format(str_number, str_new_number))
+
+        self.db.cnxn.commit()
+
+
+    def copyGroupClicked(self):
+        number = list(self.ui.groupPointerComboBox2.currentText())
+        new_number = number.copy()
+        new_number[0] = str(int(new_number[0]) + 1)
+        str_new_number = ''
+        str_number = ''
+        for i in new_number :
+            str_new_number += i
+
+        for i in number:
+            str_number += i
+        self.db.cursor.execute("INSERT INTO groups (name) VALUES ('{}'); "
+                               "INSERT INTO people (first_name, last_name, pather_name, group_id, type) "
+                               "(SELECT  first_name, last_name, pather_name, (SELECT id from groups where name = '4084/1_2018'), type from people "
+                               "WHERE group_id = (SELECT id from groups where name = '{}'));".format(str_new_number, str_number))
+
+        self.db.cnxn.commit()
 
     def addMarkClicked(self):
         student_name = str(self.ui.studentNameComboBox.currentText())
@@ -143,97 +214,6 @@ class Teacher_window(QtWidgets.QMainWindow):
                                "WHERE P.GROUP_ID = (SELECT ID FROM GROUPS WHERE NAME = '{}')".format(name))
         number = self.db.cursor.fetchone()
         self.ui.numGroupsWIthMarksLabel.setText(str(number[0]))
-        # def show_by_date(self) :
-    #     date = self.ui.date_med_record_dateEdit.date().toPyDate()
-    #     self.db.cursor.execute("SELECT date, P.name, diagnosis FROM medical_records "
-    #                            "LEFT JOIN Patients P on P.id = patient_id "
-    #                            "WHERE physician_id = (SELECT id FROM physicians WHERE user_id = '"
-    #                            + str(user_info.current_userID) + "') and date = '" + str(date) + "' ORDER BY date desc")
-    #     filler.fillTable(self.ui.med_journal_tableWidget, self.db.cursor, 3)
-    #
-    # def change_med_record(self) :
-    #     new_diagnosis = str(self.ui.new_diagnosis_textEdit.toPlainText())
-    #     self.db.cursor.execute("UPDATE Medical_records "
-    #                                "SET diagnosis = %s "
-    #                                "WHERE (id = (SELECT id FROM Medical_records WHERE physician_id = %s "
-    #                                "ORDER BY id DESC LIMIT 1))", (new_diagnosis, str(user_info.current_userID)));
-    #
-    #     self.db.cnxn.commit()
-    #     self.show_all_med_records()
-    #
-    # def show_all_med_records(self) :
-    #     self.db.cursor.execute("SELECT date, P.name, diagnosis FROM medical_records "
-    #                            "LEFT JOIN Patients P on P.id = patient_id "
-    #                            "WHERE physician_id = (SELECT id FROM physicians WHERE user_id = '"
-    #                            + str(user_info.current_userID) + "') ORDER BY date desc")
-    #     filler.fillTable(self.ui.med_journal_tableWidget, self.db.cursor, 3)
-    #
-    # def show_in_selected_date(self) :
-    #     date = self.ui.date_med_record_dateEdit.date().toPyDate()
-    #     self.db.cursor.execute("SELECT date, P.name, diagnosis FROM medical_records "
-    #                            "LEFT JOIN Patients P on P.id = patient_id "
-    #                            "WHERE physician_id = (SELECT id FROM physicians WHERE user_id = '"
-    #                            + str(user_info.current_userID) + "' and date = '" + str(date) + "') ORDER BY date desc")
-    #     filler.fillTable(self.ui.med_journal_tableWidget, self.db.cursor, 3)
-    #
-    # def patients_change(self):
-    #     print("Change patient")
-    #     self.ui.med_card_tableWidget.setRowCount(0)
-    #     current_patient = str(self.ui.patients_comboBox.currentText())
-    #
-    #     sqlcmd = ("SELECT date, Ph.name, diagnosis FROM medical_records "
-    #                            "LEFT JOIN Physicians Ph on Ph.id = physician_id "
-    #                            "WHERE patient_id = (SELECT id FROM patients WHERE name = '"
-    #                            + current_patient + "') ORDER BY date desc")
-    #     self.db.cursor.execute(sqlcmd)
-    #
-    #     filler.fillTable(self.ui.med_card_tableWidget, self.db.cursor, 3)
-    #
-    #
-    # def change_nummer_button_clicked(self):
-    #     filler.changeNummer(self)
-    #
-    # def admin_menu_button_clicked(self) :
-    #     self.menu = Admin_Controller()
-    #     self.menu.show()
-    #
-    # def exit_button_clicked(self):
-    #     filler.exitButton(self)
-    #
-    #
-    # def form_queue_button_clicked(self):
-    #     days = str(self.ui.queue_days_box.value())
-    #
-    #     self.db.cursor.execute("SELECT P.name, Q.time FROM Queue Q LEFT JOIN Patients P on P.id = Q.patient_id "
-    #                            "WHERE ((physician_id = %s)  AND (time > current_timestamp) AND (time < current_timestamp + interval %s day))",
-    #                            (user_info.current_userID, days))
-    #
-    #     filler.fillTable(self.ui.Queue_tableWidget, self.db.cursor, 2)
-    #
-    #
-    # def add_record_Button_clicked(self):
-    #     diagnosis = str(self.ui.diagnosis_textBrowser.toPlainText())
-    #     if not(diagnosis.isalnum()) and len(diagnosis) < 3 :
-    #         error_     message = QtWidgets.QErrorMessage(self)
-    #         error_message.setWindowTitle("Некорректный ввод")
-    #         error_message.showMessage('Подозрительный диагноз')
-    #     else :
-    #         name = str(self.ui.patients_comboBox.currentText())
-    #         service = str(self.ui.service_comboBox.currentText())
-    #         try:
-    #             self.db.cursor.execute("INSERT INTO medical_records(patient_id, physician_id, date, diagnosis, service_id)" \
-    #                     "VALUES ((SELECT id FROM patients WHERE name = %s), %s, %s, %s, "
-    #                     "(SELECT id FROM Services WHERE name = %s))",
-    #                     (name, user_info.current_userID, datetime.datetime.today().strftime('%Y-%m-%d'), diagnosis, service))
-    #
-    #             self.db.cnxn.commit()
-    #             self.patients_change()
-    #             print("smth happened")
-    #         except (Exception, psycopg2.Error) as error:
-    #             print("Error while connecting to PostgreSQL", error)
-    #
-    #
-
 
     def setInitialValues(self):
         teacher_id = user_info.current_userID
@@ -273,27 +253,23 @@ class Teacher_window(QtWidgets.QMainWindow):
         self.ui.typeComboBox.clear()
         self.ui.typeComboBox.addItem("Student")
         self.ui.typeComboBox.addItem("Teacher")
-        # self.db.cursor.execute(
-        #     "SELECT S.name, S.id from Physicians Ph LEFT JOIN Specialization S on Ph.specialization_id = S.id WHERE Ph.user_id=" + str(
-        #         teacher_id))
-        # spec_name = self.db.cursor.fetchone()
-        # self.ui.spezialization_label.setText(spec_name[0])
-        #
-        # self.db.cursor.execute("SELECT passport from Physicians where user_id=" + str(teacher_id))
-        # pas_name = self.db.cursor.fetchone()
-        # self.ui.passport_label.setText(str(pas_name[0]))
-        #
-        # self.db.cursor.execute("SELECT date_of_birth from Physicians where user_id=" + str(teacher_id))
-        # date_of_birth = self.db.cursor.fetchone()
-        # self.ui.date_of_birth_label.setText(str(date_of_birth[0]))
-        #
-        # self.db.cursor.execute("SELECT phone from  authentication_data  WHERE id= %s", str(user_info.current_userID))
-        # phone = self.db.cursor.fetchone()
-        # self.ui.phone_number_box.setText(str(phone[0]))
 
-        # self.db.cursor.execute("SELECT name from Services where specialization_id = %s" , str(spec_name[1]))
-        # filler.fillComboBox(self.ui.service_comboBox, self.db.cursor)
-        #
-        # self.form_queue_button_clicked()
-        # self.show_all_med_records()
+        self.db.cursor.execute("SELECT name from groups")
+        filler.fillComboBox(self.ui.groupPointerComboBox2, self.db.cursor)
 
+        self.db.cursor.execute("SELECT  p.name, s.name FROM MARKS M "
+                               "FULL JOIN PEOPLE P ON P.ID = M.STUDENT_ID "
+                               "LEFT JOIN GROUPS G ON G.ID = P.GROUP_ID "
+                               "FULL Join subject s on M.subject_id = s.id")
+        filler.fillTable(self.ui.studentSubjectTableWidget, self.db.cursor, 2)
+
+        self.db.cursor.execute("select * from avgMarkByTeacher")
+        filler.fillTable(self.ui.avgMarkByTeacherTableWidget, self.db.cursor, 3)
+
+        self.db.cursor.execute("select * from avgMarkByYears")
+        filler.fillTable(self.ui.yearMarksTableWidget, self.db.cursor, 2)
+
+        self.ui.yearComboBox.clear()
+        self.ui.yearComboBox.addItem("")
+        for i in range (2015, datetime.date.today().year) :
+            self.ui.yearComboBox.addItem(str(i))
